20b292e58e61256dedba0af55054d99be535dc40
Jhow <jhonatasrcorrea@gmail.com>
2025-09-08 14:48:42 -0300

    adicionando sistema de login a pagina admin

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<ConfigService>();
 builder.Services.AddSingleton<FileService>();
 builder.Services.AddHostedService<PrintSchedulerService>();
+builder.Services.AddSingleton<AdminConfigService>();
 
 // Ouvir em todas interfaces na porta 5000 (HTTP)
 builder.WebHost.UseUrls("http://0.0.0.0:5000"); //http://localhost:5000
---
+++ Services/AdminConfigService.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+public class AdminConfig
+{
+    [JsonPropertyName("adminUsername")]
+    public string AdminUsername { get; set; }
+
+    [JsonPropertyName("adminPassword")]
+    public string AdminPassword { get; set; }
+}
+
+public class AdminConfigService
+{
+    public AdminConfig Load()
+    {
+        var json = File.ReadAllText("adminconfig.json");
+        return JsonSerializer.Deserialize<AdminConfig>(json);
+    }
+}
