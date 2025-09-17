3b01dbe8eb4c9b99bf2e894bf8329ed0262c57d9
Jhow <jhonatasrcorrea@gmail.com>
2025-09-16 23:23:09 -0300

    Removendo comentarios de settings.cs

--- Models/Settings.cs
+++ Models/Settings.cs
@@ -4,9 +4,9 @@
     {
         public int intervaloImpressaoSegundos { get; set; } = 5;
         public int fotosPorLote { get; set; } = 2;
-        public string? caminhoPastaImpressora { get; set; } = "";  // Caminho que impressora monitora
-        public string? caminhoPastaBackup { get; set; } = "";      // Caminho para backups do fotógrafo
-        public string? titulo { get; set; } = "FotoPrint";  // Novo campo titulo com default
+        public string? caminhoPastaImpressora { get; set; } = "";  
+        public string? caminhoPastaBackup { get; set; } = "";      
+        public string? titulo { get; set; } = "FotoPrint";  
 
     }
 }
